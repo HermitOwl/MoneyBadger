24c89d270fb0438e72d1ff577d232537399128d5
LeonIrving <31895017+LeonIrving@users.noreply.github.com>
2018-03-04 11:47:26 +0000

    Asset Object 1.0.0
    First iteration of the Asset object, with functions.

--- MoneyBadger/B00310481, B00304871, B00301496, B00305100 - Prototype/budgetBackend.js
+++ MoneyBadger/B00310481, B00304871, B00301496, B00305100 - Prototype/budgetBackend.js
@@ -10,13 +10,28 @@ function Reminder(name, description, date, frequency){
 	this.date = date;
 };
 
-function Asset(name, quantity, isIncome, dateOfReccurence,frequency){
-	this.name = name;
-	this.quantity= quantity;
-	this.isIncome = isIncome;
-	this.reminder = new Reminder("","",dateOfRecurrence,frequency);
-	
-};
+// Asset Object - Leon Irving B00301496
+
+// Instantiates a new asset
+function Asset(name, quantity, isIncome, reminder){
+    this.name = name;
+    this.quantity = quantity;
+    this.isIncome = isIncome;
+    this.reminder = new Reminder("", "", new Date(), 0);
+}
+
+// Setting a new reminder
+function setReminder(name, description, date, frequency){
+	new Reminder(name, description, date, frequency);
+}
+
+// Reset a reminder
+function removeReminder(reminder){
+	reminder.name = "";
+	reminder.description = "";
+	reminder.date = new Date();
+	reminder.frequency = 0;
+}
 
 function UserBudget(userName, userPassword, filePath){
 	this.userName = userName;
